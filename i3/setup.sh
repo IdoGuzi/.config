1c36ccb2abd9107ecc804ed3ad8bcef8f503c0a5
Ido Guzi <idoguzi@hotmail.com>
2023-11-20 19:49:58 +0200

    keybind changes

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "export TERMINAL=kitty" > ~/.xinitrc
-echo "export LIBGL_ALWAYS_SOFTWARE=true" >> ~/.xinitrc
-echo "export GALLIUM_DRIVER=llvmpipe kitty" >> ~/.xinitrc
+#LIBGL_ALWAYS_SOFTWARE will run opengl on cpu
+echo "export LIBGL_ALWAYS_SOFTWARE=true" > ~/.xinitrc
+#echo "export GALLIUM_DRIVER=llvmpipe kitty" >> ~/.xinitrc
 echo "exec i3" >> ~/.xinitrc
