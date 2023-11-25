533025ba62dfc8958ef81c904327ce85dccf2a6d
Ido Guzi <idoguzi@hotmail.com>
2023-11-25 12:58:39 +0200

    terminal opacity

--- i3/i3.sh
+++ i3/i3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S git base-devel xorg-server xorg-xinit xorg-xrandr libxcursor i3 kitty dmenu feh noto-fonts ttf-nerd-fonts-symbols
+pacman -S git base-devel xorg-server xorg-xinit xorg-xrandr libxcursor i3 kitty dmenu feh noto-fonts ttf-nerd-fonts-symbols picom
 cd $(HOME)
 git clone https://aur.archlinux.org/yay.git
 cd yay
